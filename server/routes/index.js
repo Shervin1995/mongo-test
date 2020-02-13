65e3304680c1a253de7b319a85c8472e593593f8
dadashi <khanim97@gmail.com>
2020-02-13 07:19:22 +0330

    1

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,38 +3,16 @@ var express = require('express')
 var router = express.Router()
 var Numeral = require('numeral')
 var MongoClient = require('mongodb').MongoClient
+var path = require('path') 
 var db
 MongoClient.connect('mongodb://localhost:27017/',
   function(err,client){
     db = client.db('learning_mongo')
 })
 
-// Home
-router.get('/',(req,res)=>{
-			var x = `
-      <h1 style='color:purple'>Insert!</h1>
-      <form id='insertform' method="post">
-      tourPrice:<br>
-     <input id='tourPrice' type="text" name="tourPrice"><br>
-      tourDescription:<br>
-      <input id='tourDescription' type="text" name="tourDescription">
-      <br><br>
-        <input type="submit" value="insert">
-      </form>
-      <hr>
-			<h1 style='color:purple'>Show!</h1>
-
-      <div class='showlist'>
-
-      </div>
-
-      <script src="/js/jqueryajax.js"></script>
-      <script src="/js/insertAjax.js"></script>
-			`
-
-			res.send(x)
+// react (home)
+router.get('/*',function(req,res){
+  res.sendFile(path.resolve(__dirname,'../public/react.html'))
 })
-
-
 
 module.exports = router
