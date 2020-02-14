edbde68b2a0f67a32bc49bca4182f14e0f0a5512
dadashi <khanim97@gmail.com>
2020-02-14 17:52:41 +0330

    1

--- app/app1.jsx
+++ app/app1.jsx
@@ -1,19 +1,25 @@
 import React,{Component} from 'react'
 // App1
 export class App1 extends Component{
+fn23(){
+  location.reload()
+  {/* now input fields clear after each submit
+    i could also use a cleaner instead location.reload */}
+}
 
   render(){
       return(
 	  <div>
     <h1 >Insert!</h1>
-      <form id='insertform' >
+      <form onSubmit={this.fn23} id='insertform' >
       tourPrice:<br/>
-     <input id='tourPrice' type="text" name="tourPrice"/><br/>
+     <input id='tourPrice' type="text" name="tourPrice" /><br/>
       tourDescription:<br/>
       <input id='tourDescription' type="text" name="tourDescription"/>
       <br/> <br/>
         <input type="submit" value="insert"/>
       </form>
+      {/* part 2 table*/}
       <hr/>
       <h1>Show!</h1>
     <div class='showlist'>
