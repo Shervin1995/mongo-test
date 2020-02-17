0f8efaad9506c9d8aa990364e5d4205baa12f9f8
dadashi <khanim97@gmail.com>
2020-02-17 12:20:45 +0330

    1

--- app/component/table.jsx
+++ app/component/table.jsx
@@ -2,6 +2,7 @@ import React,{Component,useState,useEffect} from 'react'
 import MaterialTable from 'material-table'
 import Button from '@material-ui/core/Button'
 
+
 export function Table() {
   const [state, setState] = useState({ })
 
@@ -20,20 +21,19 @@ export function Table() {
   return (
     <MaterialTable
       title="Let's go to the Mountains!"
-      id="muiTable"
       columns={state.columns}
       data={state.data}
-      components={{
-        Action: props => (
-          <Button
-            className= 'delbutton'
-            id={dataRow._id}
-            tooltip= 'delete User'
-          >
-            Delete
-          </Button>
-        ),
-      }}
+
+      actions={[
+        {
+          icon: () => <a className='delButton' id='' >delete</a>,
+          tooltip: 'deleteTT',
+          onClick: function(event, rowData){
+            const element = this.querySelector('a')
+            element.setAttribute(id,rowData._id)
+          }
+        }
+      ]}
 
     />
   );
